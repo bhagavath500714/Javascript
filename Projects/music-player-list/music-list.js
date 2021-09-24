6ccdb1219e198f705ccf19481aaff3f8e680eb49
bhagavath26 <bhagavath.k@capestart.com>
2021-09-24 11:12:15 +0530

    Second commit.

---
+++ Projects/music-player-list/music-list.js
@@ -0,0 +1,487 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Nandriyal Thuthi Paadu",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Naan Naanagave",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Neer Ellatha Nall",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Diva Pitha Enthan",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Thasara Etharanil",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Sontham Endu Sollikolla",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Naan Naanagave Vandhirukkiren",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-7"
+  },
+  {
+    name: "Naan Ummidathil Melaana",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-8"
+  },
+  {
+    name: "Nandri Solli",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-9"
+  },
+  {
+    name: "Neer Illadha naal Ellam",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-10"
+  },
+  {
+    name: "NEERILATHE NAALELLAM",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-11"
+  },
+  {
+    name: "Neeye Nirantaram",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-12"
+  },
+  {
+    name: "En Parvai Oli",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-13"
+  },{
+    name: "Nilai illa ulagil",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-14"
+  },
+  {
+    name: "Parama Alaipin Pandhaya",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-15"
+  },
+  {
+    name: "Parathil ulla engal pidhave",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-16"
+  },
+  {
+    name: "Parathil Ulla Engal Pithave - O",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-17"
+  },
+  {
+    name: "PUTHUBELANAI THAARUM",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-18"
+  },
+  {
+    name: "Sila Nerangalil Aayathamaa",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-19"
+  },
+  {
+    name: "Siluvai Naadhar Yesuvin",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-20"
+  },
+  {
+    name: "Singa Kebiyil Naan",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-21"
+  },
+  {
+    name: "Yesuvuku Nandii",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-22"
+  },
+  {
+    name: "Traditional Songs",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-23"
+  },
+  {
+    name: "Traditional Songs - 1",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-24"
+  },
+  {
+    name: "THENILUM",
+    artist: "Premji Ebenezer",
+    img: "music-1",
+    src: "music-25"
+  },
+  {
+    name: "Thirupadham Nambi Vandhen",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-26"
+  },
+  {
+    name: "Thollai Kashtangal",
+    artist: "Premji Ebenezer",
+    img: "music-3",
+    src: "music-27"
+  },
+  {
+    name: "Um Azhagana Kangal",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-28"
+  },
+  {
+    name: "Ummai Allamal Enaku Yarundu",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-29"
+  },
+  {
+    name: "UMMAI ARATHIPPEN",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-30"
+  },
+  {
+    name: "Unga Kirubai",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-31"
+  },
+  {
+    name: "Group",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-32"
+  },
+  {
+    name: "Uyar Malaiyo",
+    artist: "John Jebaraj ",
+    img: "music-3",
+    src: "music-33"
+  },
+  {
+    name: "Yesuve Undhan Vaarthaiyaal promo",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-34"
+  },
+  {
+    name: "Yesuve Undhan Vaarthaiyal",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-35"
+  },
+  {
+    name: "YESUVIN ANATHI SNEHAM",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-36"
+  },
+  {
+    name: "Anathi Devan",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-37"
+  },
+  {
+    name: "Alagai Nirukum Yar",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-38"
+  },
+  {
+    name: "Amba pira thanam",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-39"
+  },
+  {
+    name: "Sahaya Matha",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-40"
+  },
+  {
+    name: "Yesu va undan",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-41"
+  },
+  {
+    name: "Yesuvi Namama ",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-42"
+  },
+  {
+    name: "Unga presanthil",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-43"
+  },
+  {
+    name: "Ootru Thaneerae",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-44"
+  },
+  {
+    name: "Ellam Yesuvaa",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-45"
+  },
+  {
+    name: "En Ethayam",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-46"
+  },
+  {
+    name: "Yen Koodave Erumm",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-47"
+  },
+  {
+    name: "Kattupuravin Saththam",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-48"
+  },
+  {
+    name: "Singaa Kebill Nan",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-49"
+  },
+  {
+    name: "Seru Yasuu Nathanakuu",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-50"
+  },
+  {
+    name: "Parama Alaipin Pandhaya",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-51"
+  },
+  {
+    name: "Group - 1 ",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-52"
+  },
+  {
+    name: "Yaar Ennai Kaivittalum",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-53"
+  },
+  {
+    name: "Vanthanam vanthanamaa",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-54"
+  },
+  {
+    name: "Vanaanthira Yaaththiraiyil",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-55"
+  },
+  {
+    name: "Chinna Manushanukkulla",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-56"
+  },
+  {
+    name: "Devakumara",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-57"
+  },
+  {
+    name: "En Meetpar Uyirodu",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-58"
+  },
+  {
+    name: "En nerukathile en thunaiyaneer",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-59"
+  },
+  {
+    name: "En Uyirana Yesu",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-60"
+  },
+  {
+    name: "Chinna Manushanukkulla",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-61"
+  },
+  {
+    name: "Innayolam Enne Nadathi Ente Yesu Ethra",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-62"
+  },
+  {
+    name: "Kirubai Kirubai",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-63"
+  },
+  {
+    name: "Ennil Adangadha Sthothiram",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-64"
+  },
+  {
+    name: "Enneolam Enne Nadathi",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-65"
+  },
+  {
+    name: "keaten keaten",
+    artist: "Jesus",
+    img: "music-1",
+    src: "music-66"
+  },
+  {
+    name: "Kuyavane Kuyavane",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-67"
+  },
+  {
+    name: "AdhisayangalSeigiravar",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-68"
+  },
+  {
+    name: "AKKINI",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-69"
+  },
+  {
+    name: "Alangara Vaasalale",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-70"
+  },
+  {
+    name: "At The Cross",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-71"
+  },
+  {
+    name: "Avar Endhan Sangeetham",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-72"
+  },
+  {
+    name: "Azhagae",
+    artist: "Jesus",
+    img: "music-2",
+    src: "music-73"
+  },
+  {
+    name: "Israyelin Nathan",
+    artist: "Jesus",
+    img: "music-3",
+    src: "music-74"
+  },
+  {
+    name: "Manniyum Deva",
+    artist: "Jesus",
+    img: "music-4",
+    src: "music-75"
+  },
+  {
+    name: "Pesum Deivam Iravu",
+    artist: "Jesus",
+    img: "music-5",
+    src: "music-76"
+  },
+  {
+    name: "Ummodu Selavidum",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-77"
+  },
+  {
+    name: "Ethra tholam yehavaa",
+    artist: "Jesus",
+    img: "music-6",
+    src: "music-78"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
---
+++ Projects/music-player-list/script.js
@@ -0,0 +1,258 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+mainAudio = wrapper.querySelector("#main-audio"),
+progressArea = wrapper.querySelector(".progress-area"),
+progressBar = progressArea.querySelector(".progress-bar"),
+musicList = wrapper.querySelector(".music-list"),
+moreMusicBtn = wrapper.querySelector("#more-music"),
+closemoreMusic = musicList.querySelector("#close");
+
+// load random music on page refresh
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex); // calling load music function once window loaded
+    playingSong(); 
+})
+
+// load music function
+function loadMusic(indexNumb) {
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `../../../Extra/Music/Jesus/${allMusic[indexNumb - 1].src}.mp3`;
+}
+
+// play music function
+function playMusic(){
+    wrapper.classList.add("paused");
+    playPauseBtn.querySelector("i").innerText = "pause";
+    mainAudio.play();
+}
+
+// pause music function
+function pauseMusic(){
+    wrapper.classList.remove("paused");
+    playPauseBtn.querySelector("i").innerText = "play_arrow";
+    mainAudio.pause();
+}
+
+// next music function
+function prevMusic() {
+    // decrement of index by 1
+    musicIndex--;
+    // if musicIndex is less than 1 then musicIndex will be array length so the last song will play
+    musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+
+// prev music function
+function nextMusic() {
+    // increment of index by 1
+    musicIndex++;
+    // if musicIndex is greater than array length then musicIndex will be 1 so the first song will play
+    musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
+
+// play or music button event
+playPauseBtn.addEventListener("click", () => {
+    const isMusicPaused = wrapper.classList.contains("paused");
+    // if isMusicPaused is true then call pauseMusic else call playMusic
+    isMusicPaused ? pauseMusic() : playMusic();
+    playingSong();
+});
+
+// next music btn event
+nextBtn.addEventListener("click", () => {
+    nextMusic(); // calling next music function
+});
+
+// prev music btn event
+prevBtn.addEventListener("click", () => {
+    prevMusic(); // calling previous music function
+});
+
+// update progress bar width according to music current time
+mainAudio.addEventListener("timeupdate", (e) => {
+    // console.log(e);
+    const currentTime = e.target.currentTime; // getting current time of song
+    const duration = e.target.duration; // getting total duration of song
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
+
+    let musicCurrentTime = wrapper.querySelector(".current-time"),
+        musicDuartion = wrapper.querySelector(".max-duration");
+
+    mainAudio.addEventListener("loadeddata", () => {
+       // update song total duration
+        let audioDuration = mainAudio.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        // adding 0 if sec is less than 10
+        if(totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        musicDuartion.innerText = `${totalMin}:${totalSec}`;
+    });
+
+    // update playing song current time
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    // adding 0 if sec is less than 10
+    if(currentSec < 10) {
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+// update playing song current time according to the progress bar width
+progressArea.addEventListener("click", (e) => {
+    let progressWidthval = progressArea.clientWidth; // getting width of progress bar
+    let clickedOffSetX = e.offsetX; //getting offset x value
+    let songDuration = mainAudio.duration; //getting song total duration
+
+    mainAudio.currentTime = (clickedOffSetX / progressWidthval) * songDuration;
+    playMusic(); //if pause is clicked and progress clicked calling playMusic function
+});
+
+//change loop, shuffle, repeat icon onclick
+const repeatBtn = wrapper.querySelector("#repeat-plist");
+repeatBtn.addEventListener("click", () => {
+    // first we get the innertext of the icon then we'ill change accordingly
+    let getText = repeatBtn.innerText; // getting innerText of icon
+    // let's do different change on different icon click using switch
+    switch(getText){
+        case "repeat": //if this icon is repeat then change it to repeat_one
+            repeatBtn.innerText = "repeat_one";
+            repeatBtn.setAttribute("title", "Song looped");
+            break;
+        case "repeat_one": //if this icon is repeat_one then change it to shuffle
+            repeatBtn.innerText = "shuffle";
+            repeatBtn.setAttribute("title", "Playback shuffle");
+            break;
+        case "shuffle": //if this icon is shuffle then change it to repeat
+            repeatBtn.innerText = "repeat";
+            repeatBtn.setAttribute("title", "Playlist looped");
+            break;
+    }
+
+});
+
+// above we just changed the icon le's work on what to do after the song ended
+mainAudio.addEventListener("ended", () => {
+    // according to the icon if user has set icon to loop song then we will repeat the current song 
+
+    let getText = repeatBtn.innerText; // getting innerText of icon
+    // let's do different change on different icon click using switch
+    switch(getText){
+        case "repeat": //if this icon is repeat then simply we call the next music function so the next song will play
+            nextMusic();
+            break;
+        case "repeat_one": //if this icon is repeat_one then we'll change the current playing song current time to 0 so the song will play from bigining
+            mainAudio.currentTime = 0;
+            loadMusic(musicIndex);
+            playMusic(); // calling playMusic function
+            break;
+        case "shuffle": //if this icon is shuffle then change it to repeat
+            // generating random index between the max range of array length
+            let randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+             do{
+                randIndex = Math.floor((Math.random() * allMusic.length) + 1);
+             }while(musicIndex == randIndex); // this loop run untill the next random number won't be the same of current music index
+             musicIndex = randIndex;  // passing randomIndex to musicIndex so the random song will play
+             loadMusic(musicIndex); // calling loadMusic function
+             playMusic(); // calling playMusic function
+             playingSong();
+             break;
+    }
+});
+
+//show music list onclick of music icon
+moreMusicBtn.addEventListener("click", ()=>{
+    musicList.classList.toggle("show");
+});
+closemoreMusic.addEventListener("click", ()=>{
+    moreMusicBtn.click();
+});
+
+const ulTag = wrapper.querySelector("ul");
+
+// create li according to the array length
+for (let i = 0; i < allMusic.length; i++) {
+    // let's pass the soong name, artist from the array to li
+    let liTag = `<li li-index="${i + 1}">
+        <div class="row">
+        <span>${allMusic[i].name}</span>
+        <p>${allMusic[i].artist}</p>
+        </div>
+        <audio class="${allMusic[i].src}" src="../../../Extra/Music/Jesus/${allMusic[i].src}.mp3"></audio>
+        <span id="${allMusic[i].src}" class="audio-duration">${allMusic[i].name}</span>
+        </li>`;
+    ulTag.insertAdjacentHTML("beforeend", liTag);
+
+    let liAudioDuration = ulTag.querySelector(`#${allMusic[i].src}`);
+    let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`);
+
+    // liAudioDuration select span tag which show audio total duration and liAudio Tag select audio tag which have audio source
+    // lodeddata event used to get audio total duration without playing it
+
+    liAudioTag.addEventListener("loadeddata", () => {
+        // update song total duration
+        let audioDuration = liAudioTag.duration;
+        let totalMin = Math.floor(audioDuration / 60);
+        let totalSec = Math.floor(audioDuration % 60);
+        // adding 0 if sec is less than 10
+        if(totalSec < 10) {
+            totalSec = `0${totalSec}`;
+        }
+        liAudioDuration.innerText = `${totalMin}:${totalSec}`; //passing total duation of song
+        liAudioDuration.setAttribute("t-duration", `${totalMin}:${totalSec}`); //adding t-duration attribute with total duration value
+
+    });
+}
+
+// play particular song on click
+const allLiTags = ulTag.querySelectorAll("li");
+// console.log(allLiTags);
+function playingSong(){
+    for (let j =0; j < allLiTags.length; j++) {
+        let audioTag = allLiTags[j].querySelector(".audio-duration");
+        // let's remove playing class from all li except the last one which is clicked
+        if(allLiTags[j].classList.contains("playing")) {
+            allLiTags[j].classList.remove("playing");
+
+            // let's get that audio duration value and pass to .audio-duration innertext
+            let adDuration = audioTag.getAttribute("t-duration");
+            audioTag.innerText = adDuration; //passing t-duration value to audio duration inner text 
+        }
+
+        // if there is an li tag which li-index is equal to musicIndex then this music is playing now we'll style it
+        if(allLiTags[j].getAttribute("li-index") == musicIndex){
+            allLiTags[j].classList.add("playing");
+            audioTag.innerText = "Playing";
+        }
+        // adding onclick attribute to all li tags
+        allLiTags[j].setAttribute("onclick", "clicked(this)");
+    }
+}
+
+
+// let's play song on li click
+function clicked(element) {
+    //getting li index of particular clicked li tag
+    let getLiIndex = element.getAttribute("li-index");
+    musicIndex = getLiIndex; // passing that liindex to musicIndex
+    loadMusic(musicIndex);
+    playMusic();
+    playingSong();
+}
